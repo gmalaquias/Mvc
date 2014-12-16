08f87be097b8c32e72430bf17ebb4254fa8866d3
Gabriel Malaquias <gemalaquias@gmail.com>
2014-12-16 15:10:00 -0200

    Index

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -1,11 +1,14 @@
 <?php
 namespace Controllers;
 
+use Helpers\Annotation\Annotation;
+use Helpers\ModelState;
 use Mvc\Controller;
 
 class IndexController extends Controller{
+
     public function Index(){
         
-        $this->View();
+
     }
 }
