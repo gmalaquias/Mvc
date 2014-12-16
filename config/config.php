1fe2f2d1aadfc57a1fde47aa604aece487de09d2
Gabriel Malaquias <gemalaquias@gmail.com>
2014-12-16 15:09:40 -0200

    Namespace e Destino Atributes

--- config/config.php
+++ config/config.php
@@ -21,15 +21,19 @@ define('STATIC_URL', URL . 'site/util');
 define('IMG_URL',URL.'util/img');
 
 define('FOLDER_SRC','Application');
-
 define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('HELPERS_PATH', ROOT . DS . "vendor" . DS . "Helpers" . DS);
+define('VALIDATORS_PATH', HELPERS_PATH . "Annotation" . DS . "Validators" . DS);
 define('PATH_CONTROLLER', ROOT . FOLDER_SRC . DS . 'Controllers' . DS);
 define('PATH_VIEWS', ROOT . FOLDER_SRC . DS . 'Views' . DS);
 define('PATH_MODELS', ROOT . FOLDER_SRC . DS . 'Models' . DS);
 define('PATH_AREA', ROOT . FOLDER_SRC . DS . 'Areas' . DS);
+
+
 define('NAMESPACE_CONTROLLER', 'Controllers');
 define('NAMESPACE_MODELS', 'Models');
 define('NAMESPACE_AREAS', 'Areas');
+define('NAMESPACE_VALIDATORS', 'Helpers\\Annotation\\Validators\\');
 
 /**
  * VARIAVEL DE TIMESTAMP PARA PADRÃO DE ATRIBUTOS
