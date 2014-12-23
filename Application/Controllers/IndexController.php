d6e4735513a33b811a61224690130caf19a751b8
Gabriel Malaquias <gemalaquias@gmail.com>
2014-12-23 17:09:07 -0200

    Controle exemplo post

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -8,7 +8,15 @@ use Mvc\Controller;
 class IndexController extends Controller{
 
     public function Index(){
-        
+        $model = new \stdClass();
+        $model->nome = "teste";
+        $this->View(null, $model);
+    }
 
+    /**
+     * Post Exemplo
+     */
+    public function Index_post($model){
+        $this->View(null,$model);
     }
 }
