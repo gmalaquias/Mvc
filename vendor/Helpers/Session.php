ba18355b8042103fbb1798177ba4fec256fc9e48
Gabriel Malaquias <gemalaquias@gmail.com>
2014-12-29 17:26:33 -0200

    Remove chave

--- vendor/Helpers/Session.php
+++ vendor/Helpers/Session.php
@@ -45,9 +45,11 @@ class Session{
 
     public static function get($key){
 
-        if ( isset($_SESSION[$key]) ){
+        if ( isset($_SESSION[$key]) )
             return $_SESSION[$key];
-        }
+
+        return null;
+
     }
 
     /**
