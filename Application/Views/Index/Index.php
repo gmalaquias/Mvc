e2a8879dfb1e20d68c5d4ddd07fd2a4c0c1bb5f4
Gabriel Malaquias <gemalaquias@gmail.com>
2015-01-02 15:01:49 -0200

    File Upload

--- Application/Views/Index/Index.php
+++ Application/Views/Index/Index.php
@@ -1,3 +1,6 @@
-<form action="" method="post">
-    <input type="text" name="Nome" value="<?=$model->Nome;?>"/><input type="submit"/>
+<form action="" method="post" enctype="multipart/form-data">
+    <input type="text" name="Nome" value="<?=$model->Nome;?>"/>
+    <input type="file" name="Foto" />
+    <input type="file" name="upload" />
+    <input type="submit"/>
 </form>
