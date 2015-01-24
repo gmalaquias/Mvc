93ec015f01a047598ae3b17a6b6b62d3d39138fe
Gabriel Malaquias <gemalaquias@gmail.com>
2015-01-23 22:56:30 -0200

    Index Controller

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -8,37 +8,33 @@ use Helpers\EmailHelper;
 use Helpers\ModelState;
 use Mvc\Controller;
 use Mvc\MvcException;
+use Mvc\Url;
 use UnitOfWork\UnitOfWork;
 use UnitOfWork\UnitOfWorkException;
 
-class IndexController extends Controller{
-
-    public function Index(){
+class IndexController extends Controller
+{
+    public function Index()
+    {
         $model = new Pessoa();
-        //$model->Nome = "Gabri";
 
-        ModelState::TryValidationModel($model);
-
-        var_dump($_GET);
-
-
-        $this->View(null,$model);
+        $this->View(null, $model);
     }
 
-    public function Index_post(Pessoa $model, $arquivo = null){
+    public function Index_post(Pessoa $model, $arquivo = null)
+    {
         try {
-            if(ModelState::isValid()) {
+            if (ModelState::isValid()) {
 //                $unitof = new UnitOfWork();
 //                $unitof->Insert($model);
 //                $unitof->Save();
 
                 echo "Salvo";
             }
-        }catch (\Exception $e){
-            echo $e;
+        } catch (\Exception $e) {
+            ModelState::addError($e->getMessage());
         }
 
-        $this->View(null,$model);
+        $this->View(null, $model);
     }
-
 }
