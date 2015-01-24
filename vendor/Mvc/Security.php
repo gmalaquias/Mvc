b58325d48fbe6189ac671c40ca890b0f88962520
Gabriel Malaquias <gemalaquias@gmail.com>
2015-01-23 22:59:50 -0200

    Classe responsavel pelos usuarios

---
+++ vendor/Mvc/Security.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gabriel
+ * Date: 23/01/2015
+ * Time: 20:58
+ */
+
+namespace Mvc;
+
+/**
+ * Responsavel pelo controle de usuarios do sistema
+ * Class Security
+ * @package Mvc
+ */
+class Security {
+
+    private static $_user;
+
+    private static $_pass;
+
+    private static $_sessionId;
+
+    private static $_userId;
+
+    static function Verify(){
+        
+    }
+
+} 
