cd1d5d79de564556414c8f4dba873d7e0e0c32f8
Gabriel Malaquias <gemalaquias@gmail.com>
2015-01-24 16:10:13 -0200

    Ajuste na função trim

--- vendor/Helpers/Annotation/Validators/Standard.php
+++ vendor/Helpers/Annotation/Validators/Standard.php
@@ -12,14 +12,20 @@ namespace Helpers\Annotation\Validators;
 use Helpers\ModelState;
 use Helpers\Validation;
 
-abstract class Standard implements iValidator {
-
-    public static function isValid(Attributes $object){
+abstract class Standard implements iValidator
+{
+    public static function isValid(Attributes $object)
+    {
         $name = $object->_name;
 
-        $object->_model->$name = trim($object->_model->$name );
+        if (!is_array($object->_model->$name) && !is_object($object->_model->$name) && !empty($object->_model->$name)) {
+            $object->_model->$name = trim($object->_model->$name);
 
-        if(!array_key_exists("AllowHtml", $object->_allOptions))
-            $object->_model->$name = trim(strip_tags($object->_model->$name ));
+            if (!array_key_exists("AllowHtml", $object->_allOptions))
+                $object->_model->$name = trim(strip_tags($object->_model->$name));
+
+            if($object->_model->$name == '')
+                $object->_model->$name = null;
+        }
     }
 } 
