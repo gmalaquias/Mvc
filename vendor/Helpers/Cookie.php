770b325dcb717ea108c973dafade62afb01b009a
Gabriel Malaquias <gemalaquias@gmail.com>
2015-01-31 15:41:37 -0200

    Cookie

--- vendor/Helpers/Cookie.php
+++ vendor/Helpers/Cookie.php
@@ -1,110 +1,146 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: gabriel.malaquias
- * Date: 28/01/2015
- * Time: 11:37
+ * Classe para trabalhar com dados de sessão
+ *
+ * @author Gabriel Malaquias
+ * @access public
  */
 
 namespace Helpers;
 
-//TODO: ARRUMAR CLASSE
 class Cookie
 {
+
     public $Titulo; #Titulo do Cookie
-    public $Valores; #Array - Valores do Cookie
+    public static $Valores; #Array - Valores do Cookie
     public $Tempo; #Tempo do Cookie
     private $Dominio;
     private $Path;
 
-    public function __construct($Titulo, $Valores = null, $Tempo = -1)
+    public function __construct($Titulo, $Tempo = -1)
     {
-        if($Tempo == -1) $Tempo = time()+3600*24*30;
+        if ($Tempo == -1) $Tempo = time() + 3600 * 24 * 30;
 
         $this->Titulo = $Titulo;
-        $this->Valores = is_array($Valores) ? $Valores : Array();
+        self::$Valores = ' ';
         $this->Tempo = $Tempo;
         $this->Dominio = $_SERVER['HTTP_HOST'];
         $this->Path = "/";
 
-        if(!isset($_COOKIE[$Titulo]))
-        {
-            $val = is_array($Valores) ? serialize($Valores) : true;
-            setcookie($this->Titulo, $val, $this->Tempo, $this->Path);
-        }
+        if (!isset($_COOKIE[$Titulo]))
+            return $this->setValueCookie(self::$Valores);
 
-        return $this;
+        return $this->setValueCookie($this->getCookie());
     }
 
-    public function Definir($Campo, $Valor)
+
+    /**
+     * Grava uma informação no cookie
+     * @access public
+     * @param String $key
+     * @param String $value
+     * @return void
+     */
+
+    public function set($key, $value)
     {
-        $cookie = null;
-        //Busca o cookie
-        if(isset($_COOKIE[$this->Titulo]))
-        {
-            $cookie = $_COOKIE[$this->Titulo];
-        }else
-        {
-            setcookie($this->Titulo, true, $this->Tempo, $this->Path);
-            $cookie = $_COOKIE[$this->Titulo];
-        }
-        //echo $cookie;
-        //Pega todos os valores
-        $this->Valores = unserialize($cookie);
+        $key = strtolower($key);
 
         //Seta o valor indicado
-        $this->Valores[$Campo] = $Valor;
+        self::$Valores[$key] = $value;
 
         //Serializa
-        $serializado = serialize($this->Valores);
+        $serializado = $this->ConvertToQueryString(self::$Valores);
 
         //Salva
-        setcookie($this->Titulo,$serializado, $this->Tempo, $this->Path);
-
-        return $this;
+        $this->setValueCookie($serializado);
     }
 
-    public function Ver($Campo)
+    /**
+     * Retorna um dado do cookie
+     * @access public
+     * @param String $key
+     * @return String
+     */
+
+    public function get($key)
     {
-        $Campo = strtolower($Campo);
+        $key = strtolower($key);
 
-        $cookie = $_COOKIE[$this->Titulo];
-        $this->Valores = unserialize($cookie);
-
-
-        if(isset($this->Valores[$Campo]))
-            return $this->Valores[$Campo];
+        if (isset(self::$Valores[$key]))
+            return self::$Valores[$key];
         else
             return null;
     }
 
-    public static function DefinirSimples($Titulo, $Valor, $Tempo = -1)
+    /**
+     * Deleta um dado da sessão
+     * @access public
+     * @param String $key
+     * @return void
+     */
+    public function delete($key = "")
     {
-        if($Tempo == -1) $Tempo = time()+3600*24*30;
+        $key = strtolower($key);
+        //Serializa
+        $valores  = self::$Valores;
 
-        setcookie($Titulo,$Valor, $Tempo, "/", $_SERVER['HTTP_HOST']);
-    }
-    public static function Deletar($Titulo, $Campo = ""){
-        if(isset($_COOKIE[$Titulo])) {
-            if (empty($Campo)) {
-                $_COOKIE[$Titulo] = "";
-                unset($_COOKIE[$Titulo]);
-            } else {
-                //Pega Session
-                $cookie = $_COOKIE[$Titulo];
-                //Serializa
-                $valores = @unserialize($cookie) !== FALSE ? unserialize($cookie) : Array();
-                //Verifica campo
-                if (isset($valores[$Campo])) {
-                    //Apaga valor desejado
-                    $valores[$Campo] = "";
-                    unset($valores[$Campo]);
-                    //Serializa
-                    $serializado = serialize($valores);
-                    //Salva
-                    $_COOKIE[$Titulo] = $serializado;
-                }
-            }
+        //Verifica campo
+        if (isset($valores[$key])) {
+            //Apaga valor desejado
+            $valores[$key] = "";
+            unset($valores[$key]);
+            //Serializa
+            $serializado = $this->ConvertToQueryString($valores);
+            //Salva
+           $this->setValueCookie($serializado);
         }
+    }
+
+    /**
+     * Destrói todos os dados do cookie
+     * @access public
+     * @return void
+     */
+
+    public function destroy()
+    {
+        unset($_COOKIE[$this->Titulo]);
+        setcookie($this->Titulo, '', time() - 3600, $this->Path); // empty value and old timestamp
+    }
+
+
+    private function setValueCookie($value){
+        self::$Valores = $this->ConvertToArray($value);
+        setcookie($this->Titulo, $value, $this->Tempo, $this->Path);
+    }
+
+    private function ConvertToArray($str){
+        $str = str_replace("Cookie","&", $str);
+        parse_str($str, $output);
+
+        return $output;
+    }
+
+    private function ConvertToQueryString( $array ){
+
+        $query_array = array();
+
+        if(is_array($array)) {
+            foreach ($array as $key => $key_value) {
+
+                $query_array[] = urlencode($key) . '=' . urlencode($key_value);
+
+            }
+
+            return implode('&', $query_array);
+        }
+
+        return '';
+    }
+
+    private function getCookie(){
+        return $_COOKIE[$this->Titulo];
     }
 }
