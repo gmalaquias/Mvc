fb5c6a46058b09816de802dc31d4d4498b4b2fa6
Gabriel Malaquias <gemalaquias@gmail.com>
2015-02-11 22:47:30 -0200

    select

--- vendor/UnitOfWork/Select.php
+++ vendor/UnitOfWork/Select.php
@@ -12,7 +12,8 @@ namespace UnitOfWork;
 use Helpers\ModelState;
 use Mvc\Database;
 
-class Select {
+class Select
+{
 
     /**
      * Usada para montar a query que sera executada
@@ -64,118 +65,139 @@ class Select {
      */
     private $getUnique = false;
 
-    function __construct($type, $query, $persist, $db){
+    function __construct($type, $query, $persist, $db)
+    {
         $this->type = $type;
         $this->query = $query;
         $this->persist = $persist;
         $this->db = $db;
     }
 
-    function FirstOrDefault(){
+    function FirstOrDefault()
+    {
         $this->limit = 1;
         return $this->ExecuteQuery(false);
     }
 
-    function ToList(){
+    function ToList()
+    {
         return $this->ExecuteQuery(true);
     }
 
-    function OrderBy($campo){
-        if($this->order)
-            $this->query = $this->query . ", ".$campo;
+    function OrderBy($campo)
+    {
+        if ($this->order)
+            $this->query = $this->query . ", " . $campo;
         else
-            $this->query = $this->query . " ORDER BY ".$campo;
+            $this->query = $this->query . " ORDER BY " . $campo;
 
         $this->order = true;
         return $this;
     }
 
-    function OrderByDescending($campo){
-        if($this->order)
-            $this->query = $this->query . ", ".$campo . " DESC";
+    function OrderByDescending($campo)
+    {
+        if ($this->order)
+            $this->query = $this->query . ", " . $campo . " DESC";
         else
-            $this->query = $this->query . " ORDER BY ".$campo . " DESC";
+            $this->query = $this->query . " ORDER BY " . $campo . " DESC";
 
         $this->order = true;
         return $this;
     }
 
-    function Take($take){
-        if(is_numeric($take))
+    function Take($take)
+    {
+        if (is_numeric($take))
             $this->limit = $take;
         return $this;
     }
 
-    function Skip($skip){
-        if(is_numeric($skip))
+    function Skip($skip)
+    {
+        if (is_numeric($skip))
             $this->skip = $skip;
         return $this;
     }
 
-    function Select($select){
+    function Select($select)
+    {
         $this->select = $select;
-        if($select != '*' AND count(explode(',',$select)) == 1)
+        if ($select != '*' AND count(explode(',', $select)) == 1)
             $this->getUnique = true;
         return $this;
     }
 
-    function Join($join){}
+    function Join($join)
+    {
+    }
 
-    private function ExecuteQuery($all = true){
+    private function ExecuteQuery($all = true)
+    {
         $classe = NAMESPACE_ENTITIES . $this->type;
         $query = $this->getQuery();
 
-        $result = $this->db->select($query,(class_exists ($classe) && !$this->getUnique ? $classe : ''),$all);
+        $result = $this->db->select($query, (class_exists($classe) && !$this->getUnique ? $classe : ''), $all);
         return $this->ExecutePersist($result, $all);
     }
 
-    private function getQuery(){
+    private function getQuery()
+    {
         $query = $this->query;
-        if($this->limit != null && $this->skip != null)
-            $query .= " LIMIT ".$this->skip.','.$this->limit;
-        else if($this->skip == null && $this->limit != null)
-            $query .= " LIMIT " .$this->limit;
-        else if($this->skip != null && $this->limit == null)
-            $query .= " LIMIT " .$this->skip . ', 10000000000';
+        if ($this->limit != null && $this->skip != null)
+            $query .= " LIMIT " . $this->skip . ',' . $this->limit;
+        else if ($this->skip == null && $this->limit != null)
+            $query .= " LIMIT " . $this->limit;
+        else if ($this->skip != null && $this->limit == null)
+            $query .= " LIMIT " . $this->skip . ', 10000000000';
 
-        $query = str_replace('*',$this->select,$query);
+        $query = str_replace('*', $this->select, $query);
 
         return $query;
     }
 
-    private function ExecutePersist($result, $all){
-        if($this->persist == null || $result == null)
+    private function ExecutePersist($result, $all)
+    {
+        if ($this->persist == null || $result == null)
             return $result;
 
-        $persist = explode(",",$this->persist);
+        $persist = explode(",", $this->persist);
 
         $obj = NAMESPACE_ENTITIES . $this->type;
         $obj = new $obj;
 
         $virtuals = ModelState::GetVirtuals($obj);
 
-        foreach($persist as $key => $value){
-            if(!array_key_exists("_".$value,$virtuals))
+        foreach ($persist as $key => $value) {
+            $ex = explode(".", $value);
+            $value = $ex[0];
+            if (!array_key_exists("_" . $value, $virtuals))
                 unset($persist[$key]);
         }
 
-        if(is_object($result))
+        if (is_object($result))
             $result = array($result);
 
         $count = count($result);
-        for ($i=0;$i<$count;$i++) {
+        for ($i = 0; $i < $count; $i++) {
             foreach ($persist as $k => $p) {
-                $p = "_".$p;
+                $p = "_" . $p;
+                $novoPersist = explode(".", $p);
+                if (count($novoPersist) == 2) {
+                    $p = $novoPersist[0];
+                    $novoPersist = $novoPersist[1];
+                } else {
+                    $novoPersist = null;
+                }
                 $table = $virtuals[$p]["Type"];
                 $fk = $virtuals[$p]["Fk"];
 
-
                 $unitofwork = new UnitOfWork();
-                $result[$i]->$p  = $unitofwork->Get($table, $fk . " = " . $result[$i]->$fk)->ToList();
+                $result[$i]->$p = $unitofwork->Get($table, $fk . " = " . $result[$i]->$fk, $novoPersist)->ToList();
             }
         }
 
-        if($count == 1 && $all == false)
+        if ($count == 1 && $all == false)
             return $result[0];
 
         return $result;
