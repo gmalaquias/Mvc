32c1746c281d0fff414901d4d9a8655220c822b5
Gabriel Malaquias <gemalaquias@gmail.com>
2015-02-20 16:21:40 -0200

    Ajustes

--- Application/Controllers/IndexController.php
+++ Application/Controllers/IndexController.php
@@ -3,6 +3,7 @@ namespace Controllers;
 
 use Entities\Anotacao;
 use Entities\Pessoa;
+use Helpers\Annotation\Annotation;
 use Helpers\ClassGenerator\ClassGenerator;
 use Helpers\Cookie;
 use Helpers\DateTime;
@@ -18,32 +19,7 @@ class IndexController extends Controller
 {
     public function Index()
     {
-        $cookie = new Cookie('Alcatraz');
-        $cookie->set("Nome","inome");
-
-        $uni = new UnitOfWork();
-        var_dump($uni->Get("Anotacao")
-            ->Join($uni->Get("Pessoa") ,"A.PessoaId", "P.PessoaId")
-                ->Select("P.PessoaId")
-                ->Distinct()
-                ->ToList()
-        );
-
-    }
-
-    public function Index_post(Pessoa $model, $arquivo = null)
-    {
-        try {
-            if (ModelState::isValid()) {
-
-                $unitof = new UnitOfWork();
-                $unitof->Insert($model);
-                $unitof->Save();
-            }
-        } catch (\Exception $e) {
-           echo $e;
-        }
-
-        $this->View(null, $model);
+        $i = new Annotation(new Pessoa());
+        var_dump($i->getAnnotations());
     }
 }
